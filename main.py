aaed618600b9a5c8b0ccee44c27d66438401d6a4
Laender Oliveira <dev.laenderoliveira@gmail.com>
2017-10-29 10:17:56 -0200

    add download xml

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Teste")
+from bs4 import BeautifulSoup as bs
+from requests import get
+
+# Super Lima LTDA-ME
+BASE = "https://nfstock.alterdata.com.br"
+
+
+def get_xml(keynfe):
+    response = get(f"{BASE}/Consulta/laender/Consulta/Buscar?chave={keynfe}")
+    soup = bs(response.text, "html.parser")
+    href = soup.find("a").attrs["href"]
+    response = get(f"{BASE}{href}")
+    return response.text
